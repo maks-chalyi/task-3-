0587b0c04692cf315df98060ef362f64017ddb42
maks-chalyi <jscodema@gmail.com>
2024-11-01 13:56:15 +0300

    added function calcResult

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,67 @@ import classes from './App.module.css'
 const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
 const ACTIONS = ['+', '-', '=', 'c',]
 
+function calcResult(enteredNumbers) {
+	let result = 0
+	let numbers = enteredNumbers.split('')
+
+	numbers.forEach((elem, index) => {
+
+		let indexOperator = null
+
+		if (elem === '+' || elem === '-') {
+			indexOperator = index
+		}
+
+		let operand1 = numbers.slice(0, indexOperator)
+		let operand2 = numbers.slice(indexOperator + 1)
+
+		if (elem === '+') {
+			result = Number(operand1.join('')) + Number(operand2.join(''))
+		}
+
+		if (elem === '-') {
+			result = Number(operand1.join('')) - Number(operand2.join(''))
+		}
+	})
+
+	return result
+}
+
 export default function App() {
 
 	const [digit, setDigit] = useState('') // набраные числа
-	const [isEquaiiyFlag, setIsEquaiiyFlag] = useState(false) // нажат сивол "=" или нет
+	const [isEqualityFlag, setIsEqualityFlag] = useState(false) // нажат символ "=" или нет
 
 	function handleClick(elem) {
 
+		setDigit(prev => prev + elem)
 
 		if (elem === '=') {
-			const result = eval(digit)
-			setIsEquaiiyFlag(true)
-			setDigit(`${result}`)
+			try {
+				const result = calcResult(digit)
+				setDigit(result)
+				setIsEqualityFlag(true)
+			} catch (error) {
+				setDigit('Ошибка')
+			}
 
 		} else if (elem === 'c') {
-			setDigit('')
-			setIsEquaiiyFlag(false)
-		} else {
-			setDigit(currentNum => currentNum + elem)
-			setIsEquaiiyFlag(false)
+			setIsEqualityFlag(false)
+			setDigit('0')
+		} else if (elem === '+' || elem === '-') {
+			setIsEqualityFlag(false)
+		}
+
+		if (digit[0] === '0') {
+			setDigit(elem)
 		}
 	}
-
-	console.log(digit)
 
 	return (
 		<div className={classes.calculate}>
 			<div
-				className={!isEquaiiyFlag ? `${classes.screen}` : `${classes.screen} ${classes.red}`}>
+				className={!isEqualityFlag ? `${classes.screen}` : `${classes.screen} ${classes.red}`}>
 				{digit || '0'}
 			</div>
 			<div className={classes.buttons}>
